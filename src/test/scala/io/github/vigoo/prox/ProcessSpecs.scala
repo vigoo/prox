d90d67bab3562039afdcd3013df73302c07eb24c
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2017-11-20 09:36:08 +0100

    More unit tests

--- src/test/scala/io/github/vigoo/prox/ProcessSpecs.scala
+++ src/test/scala/io/github/vigoo/prox/ProcessSpecs.scala
@@ -16,18 +16,24 @@ import syntax._
 // scalastyle:off public.member.have.type
 
 class ProcessSpecs extends Specification { def is = s2"""
-  A process can
-    executed for getting it's exit code             $simpleProcessGetExitCode
-    executed by redirecting it's output to a file   $simpleProcessFileOutput
-    executed by redirecting it's output to a stream $simpleProcessStreamOutput
-    executed by redirecting it's error to a stream  $simpleProcessStreamError
-    executed by using a stream as it's input        $simpleProcessStreamInput
-    be piped to another                             $simpleProcessPiping
-    chained by multiple piping operations           $multiProcessPiping
+  A process can be
+    executed for getting it's exit code                             $simpleProcessGetExitCode
+    executed by redirecting it's output to a file                   $simpleProcessFileOutput
+    executed by redirecting it's output to a stream                 $simpleProcessStreamOutput
+    executed by redirecting it's error to a stream                  $simpleProcessStreamError
+    executed by using a stream as it's input                        $simpleProcessStreamInput
+    piped to another                                                $simpleProcessPiping
+    chained by multiple piping operations                           $multiProcessPiping
+    chained by multiple piping operations, preserving error redir   $multiProcessPipingWithErrorRedir
+    executed by redirecting it's output to a non-byte stream        $typedProcessStreamOutput
+    executed by redirecting it's error to a non-byte stream         $typedProcessStreamError
 
   The DSL prevents
     redirecting the output twice                    $doubleOutputRedirectIsIllegal
     redirecting the input twice                     $doubleInputRedirectIsIllegal
+    redirecting the error twice                     $doubleErrorRedirectIsIllegal
+    piping a process with redirected output         $pipingRedirectedOutputIsIllegal
+    piping to a process with redirected input       $pipingToRedirectedInputIsIllegal
   """
 
   def simpleProcessGetExitCode = {
@@ -37,7 +43,7 @@ class ProcessSpecs extends Specification { def is = s2"""
       trueResult <- trueRunning.waitForExit()
       falseResult <- falseRunning.waitForExit()
     } yield (trueResult.exitCode, falseResult.exitCode)
-    program.unsafeRunSync() === (0, 1)
+    program.unsafeRunSync() must beEqualTo((0, 1))
   }
 
   def simpleProcessFileOutput = {
@@ -48,7 +54,7 @@ class ProcessSpecs extends Specification { def is = s2"""
       contents <- io.file.readAll[IO](tempFile.toPath, 1024).through(text.utf8Decode).runFoldMonoid
     } yield contents
 
-    program.unsafeRunSync() === "Hello world!\n"
+    program.unsafeRunSync() must beEqualTo("Hello world!\n")
   }
 
   def simpleProcessStreamOutput = {
@@ -59,7 +65,17 @@ class ProcessSpecs extends Specification { def is = s2"""
       contents <- running.output.through(text.utf8Decode).runFoldMonoid
     } yield contents
 
-    program.unsafeRunSync() === "Hello world!\n"
+    program.unsafeRunSync() must beEqualTo("Hello world!\n")
+  }
+
+  def typedProcessStreamOutput = {
+    val program = for {
+      running <- (Process("echo", List("Hello world!")) > text.utf8Decode[IO]).start
+      _ <- running.waitForExit()
+      contents <- running.output.runFoldMonoid
+    } yield contents
+
+    program.attempt.unsafeRunSync() must beEqualTo(Right("Hello world!\n"))
   }
 
   def simpleProcessStreamError = {
@@ -70,7 +86,18 @@ class ProcessSpecs extends Specification { def is = s2"""
       contents <- running.error.through(text.utf8Decode).runFoldMonoid
     } yield contents
 
-    program.unsafeRunSync() === "Hello"
+    program.unsafeRunSync() must beEqualTo("Hello")
+  }
+
+  def typedProcessStreamError = {
+    val target: Pipe[IO, Byte, String] = text.utf8Decode.andThen(text.lines)
+    val program = for {
+      running <- (Process("perl", List("-e", """print STDERR "Hello\nworld"""")) redirectErrorTo target).start
+      _ <- running.waitForExit()
+      contents <- running.error.runLog
+    } yield contents
+
+    program.unsafeRunSync() must beEqualTo(Vector("Hello", "world"))
   }
 
   def simpleProcessStreamInput = {
@@ -83,7 +110,7 @@ class ProcessSpecs extends Specification { def is = s2"""
       _ <- running.waitForExit()
     } yield contents.trim
 
-    program.unsafeRunSync() === "5"
+    program.unsafeRunSync() must beEqualTo("5")
   }
 
   def simpleProcessPiping = {
@@ -96,7 +123,24 @@ class ProcessSpecs extends Specification { def is = s2"""
       _ <- runningWc.waitForExit()
     } yield contents.trim
 
-    program.unsafeRunSync() === "5"
+    program.unsafeRunSync() must beEqualTo("5")
+  }
+
+  def multiProcessPipingWithErrorRedir = {
+    val errorTarget: Pipe[IO, Byte, String] = text.utf8Decode.andThen(text.lines)
+    val program = for {
+      rps <- ((Process("perl", List("-e", """print STDERR "Hello\nworld"""")) redirectErrorTo errorTarget) | (Process("sort") redirectErrorTo errorTarget) | (Process("uniq", List("-c")) redirectErrorTo errorTarget)).start
+      (runningPerl, runningSort, runningUniq) = rps
+      contents <- runningUniq.output.through(text.utf8Decode).runFoldMonoid
+      perlErrors <- runningPerl.error.runLog
+      sortErrors <- runningSort.error.runLog
+      uniqErrors <- runningUniq.error.runLog
+      _ <- runningPerl.waitForExit()
+      _ <- runningSort.waitForExit()
+      _ <- runningUniq.waitForExit()
+    } yield (perlErrors, sortErrors, uniqErrors)
+
+    program.unsafeRunSync() must beEqualTo((Vector("Hello", "world"), Vector.empty, Vector.empty))
   }
 
   def multiProcessPiping = {
@@ -110,7 +154,7 @@ class ProcessSpecs extends Specification { def is = s2"""
       _ <- runningUniq.waitForExit()
     } yield contents.lines.map(_.trim).toList
 
-    program.unsafeRunSync() === List("1 apple", "2 cat", "1 dog")
+    program.unsafeRunSync() must beEqualTo(List("1 apple", "2 cat", "1 dog"))
   }
 
   def doubleOutputRedirectIsIllegal = {
@@ -125,6 +169,16 @@ class ProcessSpecs extends Specification { def is = s2"""
 
   def doubleErrorRedirectIsIllegal = {
     illTyped("""val bad = (Process("echo", List("Hello world!")) redirectErrorTo (new File("x").toPath)) redirectErrorTo (new File("y").toPath)""")
+    ok
+  }
+
+  def pipingRedirectedOutputIsIllegal = {
+    illTyped("""val bad = (Process("echo", List("Hello world!")) > new File("x").toPath) | Process("wc", List("-w"))""")
+    ok
+  }
+
+  def pipingToRedirectedInputIsIllegal = {
+    illTyped("""val bad = Process("echo", List("Hello world!")) | (Process("wc", List("-w")) < new File("x").path)""")
     ok
   }
 }
