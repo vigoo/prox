c72e0df1e7c3a029246a9ecdba46fbad4fdd4dce
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-01-06 10:22:34 +0100

    Debug travis

--- scripts/decrypt_keys.sh
+++ scripts/decrypt_keys.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 openssl aes-256-cbc -K $encrypted_4abe426e5bff_key -iv $encrypted_4abe426e5bff_iv -in travis-deploy-key.enc -out travis-deploy-key -d
 chmod 600 travis-deploy-key;
