eb59dbbaed1c5b2805399836ab955dcfccef2d1e
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2020-01-06 10:45:50 +0100

    Trying to fix ghpages push from travis

--- scripts/decrypt_keys.sh
+++ scripts/decrypt_keys.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -x
 
 openssl aes-256-cbc -K $encrypted_4abe426e5bff_key -iv $encrypted_4abe426e5bff_iv -in travis-deploy-key.enc -out travis-deploy-key -d
-chmod 600 travis-deploy-key;
-cp travis-deploy-key ~/.ssh/id_rsa;
+chmod 600 travis-deploy-key
+eval "$(ssh-agent -s)"
+ssh-add travis-deploy-key
