f9741bce2dd920c9e6ab8cccdc1fe557cbd2f50e
Scala Steward <scala_steward@virtuslab.com>
2023-10-04 08:31:46 +0000

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=v3.0.0

--- prox-fs2/src/test/scala/io/github/vigoo/prox/tests/fs2/ProxSpecHelpers.scala
+++ prox-fs2/src/test/scala/io/github/vigoo/prox/tests/fs2/ProxSpecHelpers.scala
@@ -2,16 +2,16 @@ package io.github.vigoo.prox.tests.fs2
 
 import java.io.File
 
-import cats.effect.Blocker
 import io.github.vigoo.prox.ProxFS2
 import zio.interop.catz._
 import zio.test.{TestResult, ZSpec, testM}
 import zio.{Task, URIO}
+import cats.effect.Resource
 
 trait ProxSpecHelpers {
 
   def proxTest[Nothing, Throwable](label: String)(assertion: ProxFS2[Task] => Task[TestResult]): ZSpec[Any, scala.Throwable] = {
-    testM(label)(Blocker[Task].use { blocker =>
+    testM(label)(Resource.unit[Task].use { blocker =>
       val prox = ProxFS2[Task](blocker)
       assertion(prox)
     })
